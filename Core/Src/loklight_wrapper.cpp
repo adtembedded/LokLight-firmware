358e715d6df74e6dea03086a601568eb3d3e6738
Joop ADTE <joop@adte.nl>
2026-02-13 15:15:21 +0100

    implement noise filter for analog readings

--- Core/Src/loklight_wrapper.cpp
+++ Core/Src/loklight_wrapper.cpp
@@ -117,5 +117,23 @@ extern "C" bool dcc_hw_read_analog_direction()
 {
     // On the Loklight PCB, this pin is the DCC Sense pin.
     // It is connected to the glowbulb body (not tip) connection, which should be the right track.
-    return HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
+    // On old inductor/variac based controllers this signal will fluctuate so we implement a memory efficient filter
+    static uint8_t high_cnt = 0;
+    GPIO_PinState dcc_sense_state = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1);
+    if(dcc_sense_state == GPIO_PIN_SET)
+    {
+        if(high_cnt < 0xff)
+        {
+            high_cnt++;
+        }
+    }
+    else
+    {
+        if(high_cnt > 0)
+        {
+            high_cnt--;
+        }
+    }
+    // This last number is the noise tolerance
+    return (high_cnt > 0x1f) ? true : false;
 }
